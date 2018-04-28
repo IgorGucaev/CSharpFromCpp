a80c3daae4e8b2303a395a1deea530d55adf1078
IgorGucaev <igor.gucaev@gmail.com>
2018-04-28 15:56:52 +0300

    Add project files.

---
+++ CSharpLib/FooClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Net; // WebClient
+using System.Globalization; // CultureInfo
+
+
+public class FooClass
+{
+    public int Multiply2(int value)
+    {
+        return value * 2;
+    }
+}
